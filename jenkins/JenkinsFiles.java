e2617393e489877271b76bd8f37f4ffdd4cb0bb2
Prakash-Tiwary <pctiwary.bits@gmail.com>
2018-05-16 23:23:38 -0400

    changed 10 to 8

--- jenkins/JenkinsFiles.java
+++ jenkins/JenkinsFiles.java
@@ -2,7 +2,7 @@ package jenkins;
 
 public class JenkinsFiles {
 	public static void main(String[] args) {
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 8; i++) {
 			System.out.println("Hello World" + i);
 		}
 	}
